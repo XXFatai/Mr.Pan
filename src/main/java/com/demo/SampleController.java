541a8b20015bc912ae845264a4df85847bb28c53
Tai <Tai@Tai-PC>
2017-07-17 16:36:05 +0800

    测试类

--- src/main/java/com/demo/SampleController.java
+++ src/main/java/com/demo/SampleController.java
@@ -1,21 +1,16 @@
 package com.demo;
 
-import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.EnableAutoConfiguration;
-import org.springframework.stereotype.Controller;
 import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.RestController;
 
-@Controller
+@RestController
 @EnableAutoConfiguration
 public class SampleController {
 	
-	@RequestMapping("/home")
+	@RequestMapping("/")
 	String home(){
 		return "hello";
-	}
-	
-	public static void main(String[] args) {
-		SpringApplication.run(SampleController.class, args);
 	}
 
 }
